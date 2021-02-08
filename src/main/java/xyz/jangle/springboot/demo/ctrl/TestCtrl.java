4efa5e93df8a6c63b001f7be4c3c17e90a7c5434
bof-jangle <jangle@jangle.xyz>
2021-02-08 17:45:50 +0800

    AOP相关的代码

--- src/main/java/xyz/jangle/springboot/demo/ctrl/TestCtrl.java
+++ src/main/java/xyz/jangle/springboot/demo/ctrl/TestCtrl.java
@@ -1,15 +1,27 @@
 package xyz.jangle.springboot.demo.ctrl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import xyz.jangle.springboot.demo.service.AService;
 
 @RestController
 @RequestMapping("/testCtrl")
 public class TestCtrl {
 
+	@Autowired
+	private AService aService;
+
 	@RequestMapping("/hello.ctrl")
 	public String hello() {
 		System.out.println("just so so ...");
+		aService.testMethod("这是方法的输出");
 		return "hello";
 	}
+
+	public static void main(String[] args) {
+
+	}
+
 }
